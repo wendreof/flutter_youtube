368bfa27129a8b1eb27f20a10002406e6350885f
WLF <wendreolf@gmail.com>
2019-12-03 22:13:55 -0200

    updates ....

--- git.sh
+++ git.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#NOME DAS BRANCHS DE TRABALHO
-branch='master'
-
 git status
 
 echo "DESEJA CONTINUAR? [Ss,Nn] Padrao(S)"
@@ -14,23 +11,20 @@ case "$continuar" in
         echo 'MENSAGEM PARA COMMIT'
         read message
         git commit -m "$message"
-        #git checkout master
         git pull
-        #git checkout $branch
+       
        # echo " "
         #echo "MERGE MASTER PARA BRANCH! "
         echo " "
-        git merge master
+       
         echo "TEM CONFLITO? [Ss,Nn] Padrao(N)"
         read conflito
         case "$conflito" in  
             n|N|"")  
                 echo "Git push PROJETOS"   
-                echo ""  
-                #git checkout master
-                git pull origin master
-                #git checkout $branch
-                git push origin $branch
+                echo " "
+             
+                git push origin master
             ;;
             *)
                 echo "PROCESSO FINALIZADO - Ajuste os conflitos e realize o commit novamente."
